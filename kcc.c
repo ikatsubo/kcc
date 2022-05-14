35d9e8a49985d5e2aba2c258c73898c7a6732645
Masanori Tani <metro@uta8a.org>
2022-05-14 10:50:36 +0000

    feat(step5): 四則演算を実装

--- kcc.c
+++ kcc.c
@@ -88,6 +88,7 @@ Token *new_token(TokenKind kind, Token *cur, char *str) {
   return tok;
 }
 
+/* Tokenizer */
 // 入力文字列pをトークナイズしてトークナイズしたトークンを返す
 Token *tokenize() {
   char *p = user_input;
@@ -101,7 +102,7 @@ Token *tokenize() {
       continue;
     }
 
-    if (*p == '+' || *p == '-') {
+    if (strchr("+-*/()", *p)) {
       cur = new_token(TK_RESERVED, cur, p++);
       continue;
     }
@@ -119,6 +120,116 @@ Token *tokenize() {
   return head.next;
 }
 
+/* Parser */
+typedef enum {
+  ND_ADD,  // +
+  ND_SUB,  // -
+  ND_MUL,  // *
+  ND_DIV,  // /
+  ND_NUM,  // 整数
+} NodeKind;
+
+// AST node type
+typedef struct Node Node;
+struct Node {
+  NodeKind kind;
+  Node *lhs;  // left hand side
+  Node *rhs;  // right hand side
+  int val;    // if kind == ND_NUM の時使う
+};
+
+Node *new_node(NodeKind kind) {
+  Node *node = calloc(1, sizeof(Node));
+  node->kind = kind;
+  return node;
+}
+
+Node *new_binary(NodeKind kind, Node *lhs, Node *rhs) {
+  Node *node = new_node(kind);
+  node->lhs = lhs;
+  node->rhs = rhs;
+  return node;
+}
+
+Node *new_num(int val) {
+  Node *node = new_node(ND_NUM);
+  node->val = val;
+  return node;
+}
+
+Node *expr();
+Node *mul();
+Node *primary();
+
+Node *expr() {
+  Node *node = mul();
+
+  for (;;) {
+    if (consume('+')) {
+      node = new_binary(ND_ADD, node, mul());
+    } else if (consume('-')) {
+      node = new_binary(ND_SUB, node, mul());
+    } else {
+      return node;
+    }
+  }
+}
+
+// mul = primary ("*" primary | "/" primary)
+Node *mul() {
+  Node *node = primary();
+  for (;;) {
+    if (consume('*')) {
+      node = new_binary(ND_MUL, node, primary());
+    } else if (consume('/')) {
+      node = new_binary(ND_DIV, node, primary());
+    } else {
+      return node;
+    }
+  }
+}
+
+// primary = "(" expr ")" | num
+Node *primary() {
+  if (consume('(')) {
+    Node *node = expr();
+    expect(')');
+    return node;
+  }
+  return new_num(expect_number());
+}
+
+// Code generator
+void gen(Node *node) {
+  if (node->kind == ND_NUM) {
+    printf("  push %d\n", node->val);
+    return;
+  }
+  gen(node->lhs);
+  gen(node->rhs);
+
+  printf("  pop rdi\n");
+  printf("  pop rax\n");
+
+  switch (node->kind) {
+    case ND_ADD:
+      printf("  add rax, rdi\n");
+      break;
+    case ND_SUB:
+      printf("  sub rax, rdi\n");
+      break;
+    case ND_MUL:
+      printf("  imul rax, rdi\n");
+      break;
+    case ND_DIV:
+      printf("  cqo\n");
+      printf("  idiv rdi\n");
+      break;
+  }
+
+  printf("  push rax\n");
+}
+
 int main(int argc, char **argv) {
   if (argc != 2) {
     error("引数の個数が正しくありません");
@@ -128,28 +239,18 @@ int main(int argc, char **argv) {
   user_input = argv[1];
   // トークナイズする
   token = tokenize();
+  Node *node = expr();
 
   // アセンブリの前半部分を出力
   printf(".intel_syntax noprefix\n");
   printf(".globl main\n");
   printf("main:\n");
 
-  // 最初は数でなければならない
-  // checkしてmov命令を出力
-  printf("  mov rax, %d\n", expect_number());
+  gen(node);
 
-  // `+ <num>`, `- <num>` というトークンの並びを消費
-  // アセンブリを出力
-  while (!at_eof()) {
-    if (consume('+')) {
-      printf("  add rax, %d\n", expect_number());
-      continue;
-    }
-
-    expect('-');
-    printf("  sub rax, %d\n", expect_number());
-  }
-
+  // return rax
+  // スタックの一番上をraxとして返す
+  printf("  pop rax\n");
   printf("  ret\n");
   return 0;
 }
